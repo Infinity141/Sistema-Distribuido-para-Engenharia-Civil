66698e86dbf5094a1a681bbab2e20a7081fc833c
Andre Kenzo Naves Saijo <andre.saijo@sou.ufmt.br>
2026-05-06 19:51:31 -0400

    Create conexao.py

---
+++ ConfiguracoesMaster/conexao.py
@@ -0,0 +1,11 @@
+import redis
+from rq.decorators import job
+import cloudpickle
+
+# Conecta no banco de dados redis
+REDIS_URL = 'redis://:sua_senha@IP_DO_BROKER:6379'
+conn = redis.from_url(REDIS_URL)
+
+# Monta um mapa qual fila usar e qual serializador deve utilizar
+# O timeout='1h' garante que cálculos longos não sejam interrompidos
+tarefa_cluster = job('operacoes', connection=conn, serializer=cloudpickle, timeout=3600)
